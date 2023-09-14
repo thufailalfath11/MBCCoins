81dfeede4cc539505606885155563d4aecc783e2
thufailalfath11 <thufailalfath11@gmail.com>
2023-09-14 10:47:01 -0700

    login page edit

--- components/LoginPage.js
+++ components/LoginPage.js
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 import { TitleLogo } from "./common/Title";
 import styles from "./Login.module.css";
 import { SubmitHandler, useForm } from "react-hook-form";
+import useAuth from "@/hooks/useAuth";
 
 export default function LoginPage() {
   const [login, setLogin] = useState(false)
-  const {signIn,signUp} =useAuth() 
+  const {signIn,signUp} = useAuth() 
   const {
     register,
     handleSubmit,
